46f2c00875ee5fb4bec0b9cd138a166b85653417
Metalik <1781573+MetalikZX@users.noreply.github.com>
2025-11-26 03:00:31 +0300

    Added more UI

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
     let humanScore = 0;
     let computerScore = 0;
-    let round = 0;
-    const maxRounds = 5;
+    let round = 1;
+    const maxRounds = 6;
 
+    const gui = document.createElement('div');
+    document.body.append(gui);
+    gui.style.margin = '150px';
+
+    const roundDisplay = document.createElement('div');
+    roundDisplay.textContent = ('Choose your move!')
+
+    const yourScoreDisplay = document.createElement('div');
+    yourScoreDisplay.textContent = (`Your score: ${humanScore}`);
+
+    const ComputerScoreDisplay = document.createElement('div');
+    ComputerScoreDisplay.textContent = (`Computer score: ${computerScore}`);
+    
     const rock = document.createElement('button');
     rock.textContent = 'Rock';
-    rock.style.margin = '15px';
+    rock.style.marginRight = '15px';
     const paper = document.createElement('button');
     paper.textContent = 'Paper';
-    paper.style.margin = '15px';
+    paper.style.marginRight = '15px';
     const scissors = document.createElement('button');
     scissors.textContent = 'Scissors';
-    scissors.style.margin = '15px';
-    document.body.append(rock, paper, scissors);
+    scissors.style.marginRight = '15px';
+
+    gui.append(roundDisplay, yourScoreDisplay, ComputerScoreDisplay, rock, paper, scissors);
     
     const choiceButton = [rock, paper, scissors];
     choiceButton.toLowerCase
@@ -39,8 +53,8 @@
 }
 
 function playGame() {
-    const {result, humanChoice, computerChoice} = playRound();
-
+  const {result, humanChoice, computerChoice} = playRound();
+  roundDisplay.textContent = `Round: ${round}`;
   round++;
   console.log(`Round ${round}`);
 if (result === "Win") {
@@ -62,7 +76,11 @@ if (result === "Tie") {
     console.log ("Your score:", humanScore);
     console.log ("Computer's score:", computerScore);
 }
+yourScoreDisplay.textContent = (`Your score: ${humanScore}`);
+ComputerScoreDisplay.textContent = (`Computer score: ${computerScore}`);
+
 if (round >= maxRounds) {
+  roundDisplay.textContent = ('Game Over!')
 if (humanScore > computerScore) {
     console.log("You win the game!");
 } else if (computerScore > humanScore) {
@@ -84,6 +102,7 @@ if (
     (humanChoice === "scissors" && computerChoice === "paper")
   ) {
     result = "Win";
+    
   } else if (humanChoice === computerChoice) {
     result = "Tie";
   } else {
